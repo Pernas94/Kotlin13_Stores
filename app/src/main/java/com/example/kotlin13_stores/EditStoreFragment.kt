aeae617d86b2f75407fed7ac7952d95449457a92
Pernas94 <Pernas94@gmail.com>
2023-04-26 19:51:51 +0200

    retoques

--- app/src/main/java/com/example/kotlin13_stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/kotlin13_stores/EditStoreFragment.kt
@@ -12,10 +12,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.addTextChangedListener
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.kotlin13_stores.databinding.FragmentEditStoreBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout
 import java.util.concurrent.LinkedBlockingQueue
 
@@ -114,6 +116,32 @@ class EditStoreFragment : Fragment() {
     private fun String.editable(): Editable = Editable.Factory.getInstance().newEditable(this)
 
 
+    /**
+     * Se ejecuta cuando nuestro fragmento se enlaza con la actividad
+     * @param context Context
+     */
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+
+                MaterialAlertDialogBuilder(requireActivity())
+                    .setTitle(R.string.dialog_exit_title)
+                    .setMessage(R.string.dialog_exit_mssg)
+                    .setPositiveButton(R.string.dialog_exit_ok){_,_->
+                        if(isEnabled){
+                            requireActivity().onBackPressedDispatcher.onBackPressed()
+                            isEnabled =false
+                        }
+                    }
+                    .setNegativeButton(R.string.dialog_delete_cancel, null)
+                    .show()
+
+
+            }
+        })
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_save, menu)
         super.onCreateOptionsMenu(menu, inflater)
@@ -122,7 +150,7 @@ class EditStoreFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                mActivity?.onBackPressedDispatcher?.onBackPressed()
+                requireActivity().onBackPressedDispatcher?.onBackPressed()
 
                 //mActivity?.onBackPressed()
                 true
@@ -164,7 +192,7 @@ class EditStoreFragment : Fragment() {
                         }else{
                             mActivity?.addStore(this)
                             Toast.makeText(mActivity, R.string.edit_store_message_success, Toast.LENGTH_SHORT).show()
-                            mActivity?.onBackPressedDispatcher?.onBackPressed()
+                            requireActivity().onBackPressedDispatcher?.onBackPressed()
                         }
                     }
                 }
